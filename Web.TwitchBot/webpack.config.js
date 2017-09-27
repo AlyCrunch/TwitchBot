a46f32f7dd773c13ca91891305f12bb536828d7a
Alison JAROSZ <alison.jarosz@viacesi.fr>
2017-09-27 20:42:01 +0200

    Adds web project

---
+++ Web.TwitchBot/webpack.config.js
@@ -0,0 +1,26 @@
+﻿const { join } = require('path');
+
+module.exports = {
+    devtool: "inline-source-map",
+    entry: './src/index.ts',
+    output: {
+        filename: 'app.js',
+        path: join(__dirname, '/dist/'),
+        publicPath: '/dist/'
+    },
+    module: {
+        rules: [
+            {
+                test: /\.tsx?$/,
+                loader: "ts-loader",
+                exclude: /node_modules/
+            }
+        ]
+    },
+    resolve: {
+        extensions: [".tsx", ".ts", ".js"]
+    },
+    devServer: {
+        contentBase: __dirname
+    }
+}
